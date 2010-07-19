ef24906c93030e3b24d8828922b512e47375ddb5
Brian May <bmay@unimelb.edu.au>
2010-07-19 11:13:48 +1000

    Improve quality of packaging. Use new config scheme.

--- setup.py
+++ setup.py
@@ -42,60 +42,15 @@ def fullsplit(path, result=None):
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-code_dir = 'kgcommon'
+packages = [
+    'karaage.conf',
+]
 
-for dirpath, dirnames, filenames in os.walk(code_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-
-
-#version = __import__(code_dir).get_version()
-
-
-#media_files = []
-#for dirpath, dirnames, filenames in os.walk('media'):
-#    # Ignore dirnames that start with '.'
-#    for i, dirname in enumerate(dirnames):
-#        if dirname.startswith('.'): del dirnames[i]
-#    if '__init__.py' in filenames:
-#        packages.append('.'.join(fullsplit(dirpath)))
-#    elif filenames:
-#        media_files.append([dirpath, [os.path.join(dirpath, f) for f in filenam#es]])
-
-#for dirpath, dirnames, filenames in os.walk('templates'):
-#    # Ignore dirnames that start with '.'
-#    for i, dirname in enumerate(dirnames):
-#        if dirname.startswith('.'): del dirnames[i]
-#    if '__init__.py' in filenames:
-#        packages.append('.'.join(fullsplit(dirpath)))
-#    elif filenames:
-#        media_files.append([dirpath, [os.path.join(dirpath, f) for f in filenam#es]])
-
-
-#for i in media_files:
-#    d = i[0]
-#    i[0] = '/usr/share/kgcommon/%s' % d
-
-
-#data_files =  media_files
-#data_files.append(('/usr/sbin', ['sbin/kg-manage', 'sbin/kg-daily-cleanup']))
+data_files = [
+    ('/etc/karaage', [
+        'conf/global_settings.py',
+        'conf/ldap_attrs.py' ]),
+]
 
 setup(
     name = "karaage-common",
