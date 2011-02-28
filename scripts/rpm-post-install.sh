288db077c7f7701fa02caba07462db3cff752afa
Sam Morrison <sam@vpac.org>
2011-02-28 16:39:47 +1100

    Added post-install for RPM to create graph dirs

---
+++ scripts/rpm-post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /var/cache/karaage/graphs/projects
+mkdir -p /var/cache/karaage/graphs/institutes
+mkdir -p /var/cache/karaage/graphs/i_trends
+mkdir -p /var/cache/karaage/graphs/trends
+chown -R apache /var/cache/karaage/
+
